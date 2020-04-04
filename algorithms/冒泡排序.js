e7663c8ca0b01ee76b80f4944f32a78b8ebfb68e
yate <yateup@gmail.com>
2020-04-04 20:33:49 +0800

    Features

---
+++ algorithms/冒泡排序.js
@@ -0,0 +1,16 @@
+/**
+ * 冒泡排序
+ * 循环遍历数组，前后两个数字比较，较大数字向后排，以此类推，就像气泡上升一样
+ * 第一次循环控制循环次数，第二次循环控制循环比较的边界
+ */
+var arr = [2, 15, 47, 23, 11, 3, 20, 9, 54, 16];
+for (var i = arr.length - 1, temp; i > 0; i--) {
+  for (var j = 0; j < i; j++) {
+    temp = arr[j];
+    if (temp > arr[j+1]) {
+      arr[j] = arr[j+1];
+      arr[j+1] = temp;
+    }
+  }
+}
+console.log(arr);
---
+++ test/sum.test.js
@@ -0,0 +1,9 @@
+/**
+ * sum 方法的测试用例
+ * @type {[type]}
+ */
+const sum = require('../temp/sum');
+
+test('sum测试用例', () => {
+  expect(sum(1, 2)).toBe(3);
+});
