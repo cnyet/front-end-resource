7c492458279fafe975f04ff388c29fcc04ece2d7
yates <yateup@gmail.com>
2021-03-10 19:16:21 +0800

    fix: 🐛 remove .DS_Store

--- temp/test.js
+++ temp/test.js
@@ -1,42 +1,20 @@
-function sendRequest(urls, max, callback) {
-  var count = 0;
-  var len = urls.length;
-  var controller = new AbortController(); // 实例化一个
-  var signal = controller.signal;
-  // 取消所有请求
-  var cancel = function() {
-    controller.abort();
-    console.log('取消所有请求');
+function deepCopy(obj) {
+  if (obj === null) {
+    return null;
   }
-  var request = function() {
-    if (urls.length) {
-      count++;
-      return send(urls.shift(), count === len).then(function(res){
-        if (res === 'done') {
-          callback();
-        } else {
-          request();
-        }
-      });
-    }
-  };
-  // 1. 先并发请求 max 个url
-  for (var i = 0; i < max; i++) {
-    request();
+  if (obj instanceof Date) {
+    return new Date(obj);
   }
-  return {cancel};
+  if (obj instanceof RegExp) {
+    return new RegExp(obj);
+  }
+  if (typeof obj !== 'object') {
+    return obj;
+  }
+  var result = Array.isArray(obj) ? [] : {};
+  for (var key in obj) {
+    result[key] = deepCopy(obj[key]);
+  }
+  return result;
 }
-
-function send (url, status) {
-  return new Promise(function(resolve){
-    setTimeout(function() {
-      resolve(status ? 'done' : '');
-    }, 1000);
-  })
-}
-
-sendRequest([1,2,3,4,5], 3, function() {
-  console.log('请求已完成');
-});
-
 
